e3cd69afe201351deb23a8d8105192249bddf11e
Opilane TTHK <opilane@tthk.ee>
2024-09-25 13:11:01 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const app = require('express')()
+const port = 8080
+const req = require('express/lib/request')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./docs/swagger.json');
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+app.listen(port, () => {
+    console.log(`API up at: http://localhost:${port}`)
+})
