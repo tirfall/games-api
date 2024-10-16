3bda01522962dcd60677e4a0f7061596fc538bc0
tirfall <aleks09.teki5@gmail.com>
2024-10-16 09:47:36 +0300

    As a user I can see a list of games [Delivers #188366633]

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = 8080
+
+app.use(cors());
 const swaggerUi = require('swagger-ui-express')
 const yamljs = require('yamljs')
 const swaggerDocument = yamljs.load('./docs/swagger.yaml');
